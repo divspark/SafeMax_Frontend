d0853ffcb8bd4223a3cf2fdd4d1129003ef35b21
divspark <divyansh2004mhj@gmail.com>
2024-11-08 22:54:18 +0530

    New 3

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,37 +1,30 @@
 import React from 'react';
-import "../styles/Card.css"; // Import the CSS file for styling
+import "../styles/Card.css"; 
 import Card from './FeatureBox';
-// import FeatureBox from './FeatureBox'; 
-// import FeatureImage1 from '../images/image1.jpg'; 
-// import FeatureImage2 from '../images/image7.png';
-// import FeatureImage3 from '../images/image3.avif';
+
 
 function Features() {
   return (
-    // <div id='Features'>
-    //   <h1>Our Services</h1>
-    //   <div className='a-container'>
-    //     <FeatureBox image={FeatureImage1} title='SafeMax-Security' />
-    //     <FeatureBox image={FeatureImage2} title='Money Saving Services' /> 
-    //     <FeatureBox image={FeatureImage3} title='Usability Interface' /> 
-    //   </div>
-    // </div>
+    <div id='Features'>
+   
+    
     <div className="card-container">
       <Card
         image="image1.jpg"
-        title="Card Title 1"
+        title="SafeMax-Security"
         description="This is a longer description that will be truncated initially. Click show more to read the full text."
       />
       <Card
         image="image7.png"
-        title="Card Title 2"
+        title="Money Saving Services"
         description="This is another card with a lengthy description that will be truncated. Click show more to reveal more text."
       />
       <Card
         image="image3.avif"
-        title="Card Title 2"
+        title="Usability Interface"
         description="This is another card with a lengthy description that will be truncated. Click show more to reveal more text."
       />
+    </div>
     </div>
   );
 }
